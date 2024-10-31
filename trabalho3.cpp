8531b44f6172a1b563c191e6eaf3ff81c1ab7261
kayque_soares <kayquesoaresmarques@gmail.com>
2024-10-30 21:52:45 -0300

    Update trabalho3.cpp

--- trabalho3.cpp
+++ trabalho3.cpp
@@ -1,53 +1,102 @@
 #include <iostream>
 using namespace std;
 
-
-void insertionSort (int arr[], int size){
-    for (int i = 1; i < size; i++){
-        int key = arr[i];
+void insertionSort(int vetor[], int size) {
+    for (int i = 1; i < size; i++) {
+        int chave = vetor[i];
         int j = i - 1;
 
-        //encontra a posição correta para inserir o elemento atual
-        while (j >= 0 && arr[j] > key) {
-            arr [j + 1] = arr[j];
-            j = j - 1;
+        while (j >= 0 && vetor[j] > chave) {
+            vetor[j + 1] = vetor[j];
+            j--;
         }
-        arr[j + 1] = key;
+        vetor[j + 1] = chave;
     }
 }
 
+void bubbleSort(int vetor[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (vetor[j] > vetor[j + 1]) {
+                int temp = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = temp;
+            }
+        }
+    }
+}
 
+void selectionSort(int vetor[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        int minIndex = i;
 
-int main () {
-    const int MAX_TAMANHO = 100;
+        for (int j = i + 1; j < size; ++j) {
+            if (vetor[j] < vetor[minIndex]) {
+                minIndex = j;
+            }
+        }
+        int temp = vetor[i];
+        vetor[i] = vetor[minIndex];
+        vetor[minIndex] = temp;
+    }
+}
 
-    int arr[] = {MAX_TAMANHO};
-cout <<"Ensira a quantidade de valores que voce deseja que seja ordenado: ";
+int main() {
+    const int MAX_TAMANHO = 1000;
+    int vetor[MAX_TAMANHO];
     int tamanho;
+    cout << "Coloque a quantidade de elementos que você deseja inserir no seu modelo de organização.(1 a 20 valores, InsertionSort; 21 a 30 valores, BubbleSort; maior do que 30 valores, SelectionSort.)" << endl;
     cin >> tamanho;
 
-    for(int i = 0; i < tamanho; ++i){
-
-        cin >>  arr[i];
-
+    for (int i = 0; i < tamanho; ++i) {
+        cin >> vetor[i];
     }
-    int size = sizeof(arr) / sizeof(arr[0]);
 
-    cout << " Array original: ";
-    for(int i = 0; i < size; ++i) {
-        cout << arr[i] << " ";
+    if (tamanho <= 0) {
+        cout << "Número inválido" << endl;
+    } else if (tamanho <= 20) {
+        cout << "Vetor original: ";
 
+        for (int i = 0; i < tamanho; ++i) {
+            cout << vetor[i] << " ";
+        }
+        cout << endl;
+
+        insertionSort(vetor, tamanho);
+
+        cout << "Vetor ordenado usando Insertion Sort: ";
+        for (int i = 0; i < tamanho; ++i) {
+            cout << vetor[i] << " ";
+        }
+        cout << endl;
+    } else if (tamanho <= 30) {
+        cout << "Vetor original: ";
+        for (int i = 0; i < tamanho; ++i) {
+            cout << vetor[i] << " ";
+        }
+        cout << endl;
+
+        bubbleSort(vetor, tamanho);
+
+        cout << "Vetor ordenado usando Bubble Sort: ";
+        for (int i = 0; i < tamanho; ++i) {
+            cout << vetor[i] << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "Vetor original: ";
+        for (int i = 0; i < tamanho; ++i) {
+            cout << vetor[i] << " ";
+        }
+        cout << endl;
+
+        selectionSort(vetor, tamanho);
+        
+        cout << "Vetor ordenado usando Selection Sort: ";
+        for (int i = 0; i < tamanho; ++i) {
+            cout << vetor[i] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-
-    insertionSort(arr, size);
-
-    cout <<"Array ordenado usando Insertion Sort: ";
-    for (int i = 0; i < size; ++i) {
-        cout <<arr[i] << " ";
-
-    }
-    cout << endl;
-
     return 0;
 }
